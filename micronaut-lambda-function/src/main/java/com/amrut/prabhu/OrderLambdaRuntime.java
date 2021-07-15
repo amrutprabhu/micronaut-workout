e847e851925b6719c945de6b01db5f294d6b57db
amrutprabhu42 <amrutprabhu42@gmail.com>
2021-07-15 16:13:06 +0200

    Updated and re organised imports.

--- micronaut-lambda-function/src/main/java/com/amrut/prabhu/OrderLambdaRuntime.java
+++ micronaut-lambda-function/src/main/java/com/amrut/prabhu/OrderLambdaRuntime.java
@@ -1,17 +1,18 @@
 package com.amrut.prabhu;
+
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import io.micronaut.function.aws.runtime.AbstractMicronautLambdaRuntime;
-import java.net.MalformedURLException;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import io.micronaut.core.annotation.Nullable;
+import io.micronaut.function.aws.runtime.AbstractMicronautLambdaRuntime;
+
+import java.net.MalformedURLException;
 
 public class OrderLambdaRuntime extends AbstractMicronautLambdaRuntime<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent, Order, Order> {
 
     public static void main(String[] args) {
         try {
             new OrderLambdaRuntime().run(args);
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
